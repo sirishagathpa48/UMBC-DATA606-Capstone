8e26769278cc199321b7aaefa8514e591517d587
Sirisha Gathpa <159821456+sirishagathpa48@users.noreply.github.com>
2024-12-01 15:04:48 -0500

    Update FraudDashboard.py

--- app/FraudDashboard.py
+++ app/FraudDashboard.py
@@ -15,11 +15,6 @@ import pandas as pd
 import plotly.express as px
 from sklearn.preprocessing import StandardScaler
 
-import sklearn
-print(sklearn.__version__)
-print(st.__version__)
-print(pd.__version__)
-
 # Load your trained model and scaler
 with open('app/RandomForest_model.pkl', 'rb') as file:
     RandomForest_model = pickle.load(file)
